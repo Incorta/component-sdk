cf31d56dc8476cde28f3992dccbb835f81b3f941
Mahmoud Madkour <mahmoud.madkour@incorta.com>
2023-09-03 17:49:23 +0300

    Revert changes

--- generated/context.ts
+++ generated/context.ts
@@ -1,17 +1,7 @@
 export interface Settings {
-  applyRunTimeFilters: boolean;
-  componentType: string;
-  multiple: boolean;
-  multipleSelection: boolean;
-  vertical: boolean;
-  lov: boolean;
-  inputFields: boolean;
-  singleValue: boolean;
-  showValues: boolean;
+  [key: string]: any;
 }
 
 export interface Bindings {
-  field: {
-    dateFormat: string;
-  };
+  [key: string]: { [key: string]: any };
 }
