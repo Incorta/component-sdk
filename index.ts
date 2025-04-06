e4bb24d7faa132387deae2f2d2cece2889f52b6f
marwan <marwan.tamer@incorta.com>
2025-04-06 11:11:54 +0200

    typo fix and remote repo

--- index.ts
+++ index.ts
@@ -132,7 +132,7 @@ export function useTooltip(): {
   hideTooltip: HideTooltip;
 };
 
-export function useDadhboardState(payload: useDashboardStateProps): {
+export function useDashboardState(payload: useDashboardStateProps): {
   dashboardState: any;
   setDashboardState: setDashboardState;
 };
